03b938b3c47ffb2e12e26fef2826699ea1f29e85
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-04-12 14:03:19 +0100

    Update task.js

--- task.js
+++ task.js
@@ -1,7 +1,8 @@
-const https = require("https");
-const fs = require("fs");
+const https = require('https');
+const fs = require('fs');
 
 // Fetch data from "http://jsonplaceholder.typicode.com/posts" using https module
 const postsData = https.get('http://jsonplaceholder.typicode.com/posts');
+console.log(JSON.parse(postsData));
 
 // Write data to results/posts.json file using fs module
