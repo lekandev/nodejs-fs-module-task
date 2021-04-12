3ac6bc5025eea7c9dde0c52199e56649954edfe3
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-04-12 16:07:53 +0100

    😭😭 Done after hours of trying and faling

--- task.js
+++ task.js
@@ -1,14 +1,23 @@
-const https = require('https');
-const fs = require('fs');
+const https = require("https");
+const fs = require("fs");
+
+let data = "";
 
 // Fetch data from "http://jsonplaceholder.typicode.com/posts" using https module
-const postsData = https.get('http://jsonplaceholder.typicode.com/posts');
-console.log(JSON.parse(postsData));
+https.get("https://jsonplaceholder.typicode.com/posts", (res) => {
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
 
-// Write data to results/posts.json file using fs module
-fs.writeFile('/results/posts.json', postsData, (err) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log("data has been written to file");
+  res.on('close', () => {
+    // Write data to results/posts.json file using fs module
+    fs.writeFile("./results/posts.json", data, (err) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("data has been written to file");
+    });
+  });
 });
+
+
