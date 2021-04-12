4825573ee04b7f72c6a697b9b3c429a0572132b3
Mohammed Kabir Hussaini <65419835+lekandev@users.noreply.github.com>
2021-04-12 14:09:11 +0100

    Update task.js

--- task.js
+++ task.js
@@ -6,3 +6,9 @@ const postsData = https.get('http://jsonplaceholder.typicode.com/posts');
 console.log(JSON.parse(postsData));
 
 // Write data to results/posts.json file using fs module
+fs.writeFile('/results/posts.json', postsData, function (err,data) {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("data has been written to file");
+});
